41ceda8397438d03a1245a9c479ff8b414d69c5d
Kevin Kim <kimwooch@grinnell.edu>
2020-01-30 14:05:27 -0600

    static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,12 @@
+class StaticPagesController < ApplicationController
+    def home
+      @result = (1..6).to_a.shuffle.first
+    end
+  
+    def help
+    end
+  
+    def about 
+    end
+  end
+  
